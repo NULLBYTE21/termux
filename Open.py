8943d5853556ff7d094b689a7ed8e5afdeeeadb2
Ajeng23 <50725114+Ajeng23@users.noreply.github.com>
2019-06-15 07:22:44 +0700

    Update Open.py

--- Open.py
+++ Open.py
@@ -4,6 +4,24 @@ import sys
 import time
 from time import sleep
 
+g = "\033[32;1m"
+gt = "\033[0;32m"
+bt = "\033[34;1m"
+b = "\033[36;1m"
+m = "\033[31;1m"
+c = "\033[0m"
+p = "\033[37;1m"
+u = "\033[35;1m"
+M = "\033[3;1m"
+k = "\033[33;1m"
+kt = "\033[0;33m"
+a = "\033[30;1m"
+
+W = "\x1b[0m"
+R = "\x1b[31m"
+G = "\x1b[1;32m"
+O = "\x1b[33m"
+B = "\x1b[34m"
 P = "\x1b[35m"
 C = "\x1b[36m"
 GR = "\x1b[37m"
@@ -14,7 +32,7 @@ def slowprints(s):
         time.sleep(2.0/90)
 
 os.system("clear")
-Termux=(gt+"""
+termux=(gt+"""
  ####### ##### #####   ##    ## #    # #   #   
     #    #     #    #  # #  # # #    #  # #
     #    ##### #####   #  ##  # #    #  ##
@@ -22,7 +40,7 @@ Termux=(gt+"""
     #    ##### #     # #      #  #### #    #
  ===============================================
 """)
-slowprints(semut)
+slowprints(termux)
 print(gt+"")
 input('\nPress enter to continue...')
 sleep(2)
@@ -48,5 +66,5 @@ slowprints("\n[!] Setting up setup file...")
 sleep(2)
 os.system("termux-reload-settings")
 slowprints("[!] Successfully !! Making Termux Shortcut Key, Terimakasih sudah memakai script                        >
-os.system("rm -f key.py")
+os.system("rm -f Open.py")
 os.system('exit') 
